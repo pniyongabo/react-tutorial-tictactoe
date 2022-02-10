3fc7bdddfcc96db18334ed7a2b7f1c7d4693c82e
pniyongabo <patrickniyongabo@gmail.com>
2022-02-10 15:06:39 -0800

    added more info on individual index pages

--- react-tutorial-router/src/index.js
+++ react-tutorial-router/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
 const rootElement = document.getElementById("root");
 
@@ -12,7 +13,10 @@ render(
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          {/* invoiceId is the URL parameter  */}
+          <Route path=":invoiceId" element={<Invoice />} />{" "}
+        </Route>
         <Route
           path="*"
           element={
