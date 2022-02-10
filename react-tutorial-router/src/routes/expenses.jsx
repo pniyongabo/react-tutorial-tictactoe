d1dce42034570128d0af55ff046a51fa08d77bb2
pniyongabo <patrickniyongabo@gmail.com>
2022-02-10 14:40:18 -0800

    added 'react-tutorial-router' files

---
+++ react-tutorial-router/src/routes/expenses.jsx
@@ -0,0 +1,7 @@
+export function Expenses() {
+  return (
+    <main>
+      <h2>Expenses</h2>
+    </main>
+  );
+}
---
+++ react-tutorial-router/src/routes/invoives.jsx
@@ -0,0 +1,7 @@
+export function Invoices() {
+  return (
+    <main>
+      <h2>Invoices</h2>
+    </main>
+  );
+}
